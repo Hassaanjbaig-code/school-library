1d550ee8740bd5f96ee8a3f296ce1f9e3c338e50
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-07 10:39:56 +0500

    Udate ther person and fix linter

--- person.rb
+++ person.rb
@@ -1,25 +1,24 @@
 class Person
-    attr_reader :id, :name, :age
-    attr_writer :name, :age
+  attr_accessor :name, :age
+  attr_reader :id
 
 
-    def initialize(name: "Unknown", age: null, parent_permission: true)
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  def initialize(name: 'Unknown', age: null, parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    private 
+  private
 
-    def is_of_age?
-        @age >= 18
-    end
+  def of_age?
+    @age >= 18
+  end
 
-    public
+  public
 
-    def can_use_services?
-        is_of_age? || @parent_permission
-    end
-
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
 end
