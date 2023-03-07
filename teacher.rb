7a8537dbcbd7cbc3ccaea803b5f557bfbbd3c34f
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-07 10:48:47 +0500

    Fix linter

--- teacher.rb
+++ teacher.rb
@@ -1,11 +1,12 @@
-require_relative './person.rb' # Require the person.rb file
+require_relative './person' # Require the person.rb file
 
 class Teacher < Person
-    def initialize(specialization, name: "Unknown", age: null, parent_permission: true)
-        super(name: name, age: age, parent_permission: parent_permission)
-        @specialization = specialization
-    end
-    def can_use_services?
-        return true
-    end
+  def initialize(specialization, name: 'Unknown', age: null, parent_permission: true)
+    super(name: name, age: age, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
 end
