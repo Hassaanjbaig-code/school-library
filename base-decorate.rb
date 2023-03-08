16e4d6cfae3120339d2f5406dbec69ffd3c52686
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-08 16:51:45 +0500

    Add Base-decorate

---
+++ base-decorate.rb
@@ -0,0 +1,11 @@
+require_relative './nameable.rb'
+
+class Decorate < Nameable
+    def initialize(nameable)
+        @nameable =nameable
+        super()
+    end
+    def correct_name
+        @nameable.correct_name
+    end
+end
