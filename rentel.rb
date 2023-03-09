11d987bd4dbb7d2288af462e0f655ccb8163a08d
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-09 18:09:00 +0500

    Many to many

--- rentel.rb
+++ rentel.rb
@@ -1,7 +1,13 @@
 class Rental
-  attr_accessor :date
+  attr_accessor :date, :book, :person
 
-  def initialize(date)
+  def initialize(date, book, person)
     @date = date
+
+    @book = book
+    book.add_rental << self
+
+    @person = person
+    person.add_rental << self
   end
 end
