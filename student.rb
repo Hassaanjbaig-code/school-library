00a6ce8935c498ed2475639c994efa811ce4ca15
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-09 17:54:40 +0500

    a student belongs to a classroom

--- student.rb
+++ student.rb
@@ -1,17 +1,16 @@
 require_relative './person' # Require the person.rb file
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(classroom, age, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
-  def classroom
+  def classroom=(classroom)
     @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
-    end
+    classroom.student.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
