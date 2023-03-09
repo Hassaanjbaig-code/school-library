75642502ced9a17f0e1f90d1b0d8be9ae8eedb78
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-09 17:45:06 +0500

    has-many and belong-to relationship between classroom and student

--- student.rb
+++ student.rb
@@ -1,9 +1,17 @@
 require_relative './person' # Require the person.rb file
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, age, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
+  end
+
+  def classroom
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+    end
   end
 
   def play_hooky
